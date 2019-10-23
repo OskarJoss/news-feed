695cd5a23089837d7f6dba8f80544938de3f6352
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-10-23 13:52:48 +0200

    added function to sort articles by date

--- functions.php
+++ functions.php
@@ -2,5 +2,33 @@
 
 declare(strict_types=1);
 
-// This is the file where you can keep all your functions. Remember to NOT
-// execute/run any functions in this file. Keep it dumb.
+/**
+ * Sorts all articles by date and returns an array with the newest article first at index 0.
+ *
+ * @param array $articles
+ * @return array
+ */
+function sortArticles(array $articles): array
+{
+    $result = [];
+
+    // $i represents how many days old an article is and checks from 0-100 days.
+    for ($i = 0; $i <= 100; $i++) {
+
+        //inner loop, adds articles to result array by order of days old, starting with 0.
+        foreach ($articles as $article) {
+
+            $currentDate = date('Ymd');
+            $publishedDate = $article['published_date'];
+            $daysOld = $currentDate - $publishedDate;
+
+            if ($daysOld === $i) {
+                $result[] = $article;
+            }
+
+        }
+    }
+
+    return $result;
+}
+
