5865937207ff8a32e45ac60fa886e22aea925c11
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-10-23 11:21:06 +0200

    added basic structure of site

---
+++ data.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+$articles = [
+    [
+        'title' => 'Hej',
+        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
+        'author_id' => 1,
+        'published_date' => 20191022,
+        'likes' => 4
+    ],
+    [
+        'title' => 'Hej2',
+        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
+        'author_id' => 1,
+        'published_date' => 20191023,
+        'likes' => 2
+    ],
+];
+
+$authors = [
+    [
+        'id' => 1,
+        'full_name' => 'Kalle Anka'
+    ]
+];
