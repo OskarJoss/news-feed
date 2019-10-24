dbbcccbf569ebd1c32835bc308e7387a093c5d3a
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-10-24 13:46:26 +0200

    updated articles array

--- data.php
+++ data.php
@@ -4,72 +4,82 @@ declare(strict_types=1);
 
 $articles = [
     [
-        'title' => 'Hej',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
+        'title' => 'Now which one was it, Greg or Craig?',
+        'content' => "Mother, with Marty's parents out of town, don't you think he oughta spend the night, after all, Dad almost killed him with the car. Hey. Take that you mutated son-of-a-bitch. My pine, why you. You space bastard, you killed a pine. he's an idiot, comes from upbringing, parents were probably idiots too. Lorraine, if you ever have a kid like that, I'll disown you. Which one's your pop?
+        Of course, the Enchantment Under The Sea Dance they're supposed to go to this, that's where they kiss for the first time. You're gonna break his arm. Biff, leave him alone. Let him go. Let him go. What, right here right now in the cafeteria? What is she said no? I don't know if I could take that kind of rejection. Besides, I think she'd rather go with somebody else. Yeah, well, how about my homework, McFly? Now, Biff, um, can I assume that your insurance is gonna pay for the damage?",
         'author_id' => 1,
         'published_date' => '20191011',
         'likes' => 4
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
+        'title' => 'I just wanna use the phone.',
+        'content' => "Yeah, but I never picked a fight in my entire life. Believe me, Marty, you're better off not having to worry about all the aggravation and headaches of playing at that dance. Right, George. Well, good luck you guys. Oh, one other thing, if you guys ever have kids and one of them when he's eight years old, accidentally sets fire to the living room rug, be easy on him. Why is she gonna get angry with you? Just relax now Calvin, you've got a big bruise on you're head.
+        Alright, okay. Alright, there she is, George. Just go in there and invite her. Save the clock tower, save the clock tower. Mayor Wilson is sponsoring an initiative to replace that clock. Thirty years ago, lightning struck that clock tower and the clock hasn't run since. We at the Hill Valley Preservation Society think it should be preserved exactly the way it is as part of our history and heritage. Uh, yeah. What, what, ma? Check out that four by four. That is hot. Someday, Jennifer, someday. Wouldn't it be great to take that truck up to the lake. Throw a couple of sleeping bags in the back. Lie out under the stars.",
         'author_id' => 1,
         'published_date' => '20191019',
         'likes' => 7
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
-        'author_id' => 1,
+        'title' => 'Are those my clocks I hear?',
+        'content' => "Thank god I still got my hair. What on Earth is that thing I'm wearing? Because, you might regret it later in life. Safe now, everything's let lined. Don't you lose those tapes now, we'll need a record. Wup, wup, I almost forgot my luggage. Who knows if they've got cotton underwear in the future. I'm allergic to all synthetics. You know what I do in those situations? That's right, he's gonna be mayor.
+        Whoa, wait a minute, Doc, are you telling me that my mother has got the hots for me? Indeed I will, roll em. I, Doctor Emmett Brown, am about to embark on an historic journey. What have I been thinking of, I almost forgot to bring some extra plutonium. How did I ever expect to get back, one pallet one trip I must be out of my mind. What is it Einy? Oh my god, they found me, I don't know how but they found me. Run for it, Marty. Marty, you seem so nervous, is something wrong? Indeed I will, roll em. I, Doctor Emmett Brown, am about to embark on an historic journey. What have I been thinking of, I almost forgot to bring some extra plutonium. How did I ever expect to get back, one pallet one trip I must be out of my mind. What is it Einy? Oh my god, they found me, I don't know how but they found me. Run for it, Marty. Yeah, I'll keep that in mind.",
+        'author_id' => 2,
         'published_date' => '20190918',
         'likes' => 32
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
-        'author_id' => 1,
+        'title' => 'Last night, Darth Vader came down from planet Vulcan. And he told me that if I didn\'t take Lorraine, that he\'d melt my brain.',
+        'content' => "Well, bring her along. This concerns her too. What? Well uh, good, fine. Hey, Doc? Doc. Hello, anybody home? Einstein, come here, boy. What's going on? Wha- aw, god. Aw, Jesus. Whoa, rock and roll. Yo It works, ha ha ha ha, it works. I finally invent something that works.
+        The car, Dad, I mean He wrecked it, totaled it. I needed that car tomorrow night, Dad, I mean do you have any idea how important this was, do you have any clue? Listen, this is very important, I forgot my video camera, could you stop by my place and pick it up on your way to the mall? Just turn around, McFly, and walk away. Are you deaf, McFly? Close the door and beat it. Hi, it's really a pleasure to meet you. Hey beat it, spook, this don't concern you.",
+        'author_id' => 2,
         'published_date' => '20191014',
         'likes' => 41
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
-        'author_id' => 1,
+        'title' => 'Marty. Marty. Marty.',
+        'content' => "Oh, then I wanna give her a call, I don't want her to worry about you. yes, Joey just loves being in his playpen. he cries whenever we take him out so we just leave him in there all the time. Well Marty, I hope you like meatloaf. Really. I hope you don't mind but George asked if he could take me home. I said the keys are in here.
+        That ain't no airplane, look. I'm gonna ram him. Alright, alright, okay McFly, get a grip on yourself. It's all a dream. Just a very intense dream. Woh, hey, listen, you gotta help me. You broke it. Wow, look at him go. What did you say?",
+        'author_id' => 3,
         'published_date' => '20191005',
         'likes' => 17
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
-        'author_id' => 1,
+        'title' => 'He laid out Biff in one punch. I never knew he had it in him. He never stood up to Biff in his life.',
+        'content' => "Good. Have a good trip Einstein, watch your head. The appropriate question is, weren't the hell are they. Einstein has just become the world's first time traveler. I sent him into the future. One minute into the future to be exact. And at exactly 1:21 a.m. we should cat h up with him and the time machine. I know, and all I could say is I'm sorry. Indeed I will, roll em. I, Doctor Emmett Brown, am about to embark on an historic journey. What have I been thinking of, I almost forgot to bring some extra plutonium. How did I ever expect to get back, one pallet one trip I must be out of my mind. What is it Einy? Oh my god, they found me, I don't know how but they found me. Run for it, Marty. Uh, yeah.
+        The way I see it, if you're gonna build a time machine into a car why not do it with some style. Besides, the stainless, steel construction made the flux dispersal- look out. You bet. Uh, no, no, no, no. What are you looking at, butt-head? Doc, you gotta help me. you were the only one who knows how your time machine works. Erased from existence.",
+        'author_id' => 3,
         'published_date' => '20191023',
         'likes' => 211
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
-        'author_id' => 1,
+        'title' => 'Oh no, don\'t touch that. That\'s some new specialized weather sensing equipment.',
+        'content' => "C'mon, Mom, make it fast, I'll miss my bus. Hey see you tonight, Pop. Woo, time to change that oil. Well, safe and sound, now, n good old 1955. Uh, well, actually, I figured since it wasn't due till Monday- This is uh, this is heavy duty, Doc, this is great. Uh, does it run on regular unleaded gasoline? Yeah, I think maybe you do.
+        This is more serious than I thought. Apparently your mother is amorously infatuated with you instead of your father. Ahh. Yeah I know, If you put your mind to it you could accomplish anything. Thank god I still got my hair. What on Earth is that thing I'm wearing? He's a very strange young man.",
+        'author_id' => 4,
         'published_date' => '20190923',
         'likes' => 293
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
-        'author_id' => 1,
+        'title' => 'Looks like a airplane, without wings.',
+        'content' => "Doc, I'm from the future. I came here in a time machine that you invented. Now, I need your help to get back to the year 1985. C'mon, c'mon. Calm down, Marty, I didn't disintegrate anything. The molecular structure of Einstein and the car are completely intact. This is more serious than I thought. Apparently your mother is amorously infatuated with you instead of your father. He's alright.
+        Marty, don't go this way. Strickland's looking for you. If you're caught it'll be four tardies in a row. Well, safe and sound, now, n good old 1955. I don't like her, Marty. Any girl who calls a boy is just asking for trouble. Science Fiction Theater. Why that's me, look at me, I'm an old man.",
+        'author_id' => 4,
         'published_date' => '20191001',
         'likes' => 27
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
-        'author_id' => 1,
+        'title' => 'Hey George, heard you laid out Biff, nice going.',
+        'content' => "Damn, where is that kid. Damn. Damn damn. You're late, do you have no concept of time? Breakfast. I guess you guys aren't ready for that yet. But your kids are gonna love it. Hi, it's really a pleasure to meet you. Brown, Brown, Brown, Brown, Brown, great, you're alive. Do you know where 1640 Riverside-
+        Good evening, I'm Doctor Emmet Brown, I'm standing here on the parking lot of- That's right, twenty five years into the future. I've always dreamed on seeing the future, looking beyond my years, seeing the progress of mankind. I'll also be able to see who wins the next twenty-five world series. Oh, just a little weather experiment. The keys are in the trunk. No, fine, no , good, fine, good.",
+        'author_id' => 5,
         'published_date' => '20190822',
         'likes' => 143
     ],
     [
-        'title' => 'Hej2',
-        'content' => 'blabla dblebkeo aeofoaejf aeifo aoefboabefob afbnaoefbobae aeofioef',
-        'author_id' => 1,
+        'title' => 'Doc, she didn\'t even look at him.',
+        'content' => "You guys, take him in back and I'll be right there. Well c'mon, this ain't no peep show. Marty, you made it. Hey man, look at Marvin's hand. He can't play with his hands like that, and we can't play without him. Holy shit. Re-elect Mayor Goldie Wilson. Progress is his middle name.
+        I need fuel. Go ahead, quick, get in the car. Yeah, he's right here. Hey man, the dance is over. Unless you know someone else who could play the guitar. Alright, I'm ready. Ah, honey, your first novel.",
+        'author_id' => 5,
         'published_date' => '20190927',
         'likes' => 11
     ]
